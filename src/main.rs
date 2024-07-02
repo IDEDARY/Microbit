98e5a12903d8455e61b50f8aad10cf0ce234920a
IDEDARY <IDEDARY@gmail.com>
2024-07-02 09:46:24 +0200

    working

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,21 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
-use microbit as _;
+use rtt_target::{rtt_init_print, rprintln};
+use panic_rtt_target as _;
+use microbit::board::Board;
+use microbit::hal::timer::Timer;
+use microbit::hal::prelude::*;
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    rtt_init_print!();
+    let mut board = Board::take().unwrap();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let mut timer = Timer::new(board.TIMER0);
+
+    loop {
+        timer.delay_ms(1000u16);
+        rprintln!("1000 ms passed - Hello EdHouse");
+    }
 }
